7d029a62f758658f4df87848c6585809125ec03f
Manoj Modhwadia <manojmodhwadia@outlook.com>
2018-08-30 12:46:32 +0100

    added enableProof field to facebookAuth to include app secret proof

--- config/auth.js
+++ config/auth.js
@@ -8,7 +8,8 @@ module.exports = {
         'clientSecret'  : '60cf47519ec8419f08be00399a250010', // your App Secret
         'callbackURL'   : 'https://salty-basin-64289.herokuapp.com/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-        'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+        'profileFields' : ['id', 'email', 'name'], // For requesting permissions from Facebook API
+        'enableProoof'  : true
     },
 
     'twitterAuth' : {
