bb11d36d06577b0e80060d310f03fe021a07d288
Manoj Modhwadia <manojmodhwadia@outlook.com>
2018-09-14 14:49:11 +0100

    able to check if user has already submitted predictions for this week

--- app/game-routes.js
+++ app/game-routes.js
@@ -7,7 +7,7 @@ const router          = express.Router();
 
 const user            = require('../app/models/user');
 const { results }     = require('../app/models/results');
-const { predictions } = require('../app/models/prediction');
+const { Predictions } = require('../app/models/prediction');
 const configAuth      = require('../config/auth');
 
 
@@ -15,6 +15,7 @@ module.exports = function(router) {
 
     router.get('/checkPredictions', function(req,res) {
         // go out to the API and get the current state of things
+        console.log('started checking predictions');
         requestPromise({
             'method'  : 'GET',
             'uri'     : 'https://football-data.org/v2/competitions/PL/matches',
@@ -25,6 +26,7 @@ module.exports = function(router) {
             'rejectUnauthorized' : false
         })
         .then(data => {
+            console.log('api request complete');
             if (!data) {
                 const message = ('No Footy Data');
                 console.error(message);
@@ -35,13 +37,14 @@ module.exports = function(router) {
             let numberofPredictions = 0;
             let completedPredictions = 0;
             // go grab all Predictions for this matchDay
-            predictions.find({ matchday : matchday }, function(err, predictions) {
+            Predictions.find({ matchDay : matchday }, function(err, predictions) {
                 //console.log('predictions calc:', predictions)
                 // loop through all predictions
                 numberofPredictions = predictions.length;
+                console.log('found predictions,',numberofPredictions);
                 for (let i = 0; i < predictions.length; i++) {
                     for (let j = 0; j < data.matches.length; j++) {
-                        if (data.matches[j].matchday == matchday) {
+                        if (data.matches[j].matchDay == matchday) {
                             fixture = search(data.matches[j].homeTeam.name, data.matches[j].awayTeam.name, predictions[i].fixtures);
                             if (data.matches[j].score.winner == predictions[i].fixtures[fixture].prediction) {
                                 predictions[i].fixtures[fixture].winner = data.matches[j].score.winner; // HOME_TEAM, AWAY_TEAM, DRAW
@@ -55,7 +58,7 @@ module.exports = function(router) {
             });
 
             function search(homeTeam, awayTeam, myArray) {
-                for (var i=0; i < myArray.length; i++) {
+                for (let i = 0; i < myArray.length; i++) {
                     if (myArray[i].home_team === homeTeam && myArray[i].away_team === awayTeam) {
                         return i;
                     }
@@ -92,32 +95,51 @@ module.exports = function(router) {
 
         }*/
         let userIds = [];
-        let total = 0;
+        let userPoints = {};
+        // initialize userIds list and userPoints object
+        let users1;
         user.find({}).exec()
-            .then(function(users){
+            .then(function(users) {
+                // find all the users and push the user._id to the userIds empty []
+                // then assign each user._id as the property in userPoints object with 0 points
+                // as the value
+                users1 = users;
                 users.forEach(function(user) {
                     userIds.push(user._id);
+                    userPoints[user._id] = 0; /* {123:0,456:0,789:0} */
                 });
-
+                // return all the predictions found with a user._id in the userIds list
+                return Predictions.find({ user_id : { $in  :userIds } });
+            })
+            .then(function(predictions) {
+                // loop through all predictions
                 for (let i = 0; i < predictions.length; i++) {
-                    for (let j = 0; j < userIds.length; j++) {
-                        if (predictions[i].user_id === userIds[j] && predictions[i].result === 1) {
-                            total++;
-                            user[j].points = total;
-                            user[j].save();
-                            const message = ('points updated');
-                            return res.send(200).send(message);
-                        }
+                    // loop through every fixture in each prediction
+                    for (let j = 0; j < predictions[i].fixtures.length; j++) {
+                        // aggregating all the points in the result property for each userId
+                        userPoints[predictions[i].user_id] += predictions[i].fixtures[j].result;
+                        console.log(userPoints);
                     }
                 }
-                //return Predictions.find({user_id:{$in:userIds}})
+                users1.forEach(function(user) {
+                    // update the points prop on each user obj then save
+                    user.points = userPoints[user._id];
+                    user.save();
+                    console.log('user: '+user._id+' saved');
+                });
+                console.log('finished');
+                res.send(200);
             });
+
+
+
+
     });
     // =========================
     // LEADERBOARD PAGE
     // =========================
     router.get('/leaderboard', function(req, res) {
-        user.find({}).sort({points : 'desc'})
+        user.find({'local.userName' : req.body.userName }).exec().sort({points : 'desc'}).limit(20)
         .then(data => {
             console.log('leaderboard:', data);
             res.render('game-pages/leaderboard', { data : data });
@@ -132,6 +154,84 @@ module.exports = function(router) {
     // PREDICTIONS PAGE
     // =========================
     router.get('/predictions', function(req, res) {
+        debugger
+        requestPromise({
+            'method'  : 'GET',
+            'uri'     : 'https://football-data.org/v2/competitions/PL/matches',
+            'json'    : true,
+            'headers' : {
+                'X-Auth-Token' : configAuth.footballToken
+            },
+            'rejectUnauthorized': false
+        })
+        .then(data => {
+            if (!data) {
+                const message = ('No Footy Data');
+                console.error(message);
+                return res.status(404).send(message);
+            }
+            console.log(data.competition.name);
+            const matchday = data.matches[0].season.currentMatchday;
+            let thisWeeksFixtures = [];
+            console.log('Weeks Fixtures:', thisWeeksFixtures);
+            for (let i = 0; i < data.matches.length; i++) {
+                if (data.matches[i].matchday == matchday) {
+                    thisWeeksFixtures.push(data.matches[i]);
+                }
+            }
+            res.render('game-pages/predictions', { data : thisWeeksFixtures });
+        })
+        .catch(err => res.status(500).json({ message: 'Cannot display predictions page' }));
+    });
+    router.post('/predictions', isLoggedIn, function(req, res) {
+        requestPromise({
+            'method'  : 'GET',
+            'uri'     : 'https://football-data.org/v2/competitions/PL/matches',
+            'json'    : true,
+            'headers' : {
+                'X-Auth-Token' : configAuth.footballToken
+            },
+            'rejectUnauthorized': false
+        })
+        .then(data => {
+            if (data) {
+                console.log(req.body);
+                console.log(req.user);
+                const matchday = data.matches[0].season.currentMatchday;
+                console.log('matchday:', matchday);
+                Predictions.find({ user_id : req.user_id, matchDay : matchday }, function(err, prediction) {
+                    if (prediction) {
+                        res.render('game-pages/update-predictions');
+                        // req.flash('You have already made predictions for this weeks fixtures but you can update them here');
+                    }
+                    let fixtures = [];
+                    for (let i = 0; i < 10; i++) {
+                        fixtures.push({
+                            home_team  : req.body["fixtureHome_" + (i + 1)],
+                            away_team  : req.body["fixtureAway_" + (i + 1)],
+                            prediction : req.body["fixture_" + (i + 1)]
+                        });
+                    }
+                    Predictions.create({
+                        user_id: req.user._id,
+                        matchDay: req.body.matchDay,
+                        fixtures: fixtures
+                    }),
+                    function(err, prediction) {
+                        if(!err) {
+                            console.log('prediction:', prediction);
+                            res.render('game-pages/predictions-posted.ejs');
+                        }
+                    };
+                });
+            }
+            const message = ('No Footy Data');
+            console.error(message);
+            return res.status(404).send(message);
+        })
+        .catch(err => res.status(500).json({ message: 'Cannot display predictions page' }));
+    });
+    router.put('game-pages/update-predictions/', isLoggedIn, function(req, res) {
         requestPromise({
             'method'  : 'GET',
             'uri'     : 'https://football-data.org/v2/competitions/PL/matches',
@@ -148,40 +248,23 @@ module.exports = function(router) {
                 return res.status(404).send(message);
             }
             const matchday = data.matches[0].season.currentMatchday;
-            console.log('matchday:', matchday);
-            return requestPromise({
-                'method'  : 'GET',
-                'uri'     : 'https://football-data.org/v2/competitions/PL/matches',
-                'qs'      : {
-                    'matchday' : `${matchday}`
-                },
-                'json'    : true,
-                'headers' : {
-                    'X-Auth-Token' : configAuth.footballToken
-                },
-                'rejectUnauthorized': false
-            })
-            .catch(err => res.status(500).json({ message : 'Gone Pete Tong' }));
-        })
-        .then(data => {
-            res.render('game-pages/predictions.ejs', { data : data });
-        });
-        console.log('predictions');
-    });
-    router.post('/predictions', function(req, res) {
-        console.log(req.body);
-        for (let i = 0; i < 10; i++) {
-            predictions.create({
-                home_team  : req.body.fixtureHome[i + 1],
-                away_team  : req.body.fixtureAway[i + 1],
-                prediction : req.body.fixture_[i + 1]
+            Predictions.findOne({ user_id : req.user_id, matchDay : matchday },
+            function(err, prediction) {
+                if (!prediction) {
+                    req.flash('You have\'nt made a prediction for this week to update, Please make one now');
+                    res.render('game-pages/predictions.ejs');
+                    //res.render('game-pages/predictions')
+                }
+                const thisWeeksFixtures = [];
+                console.log('empty update list:', thisWeeksFixtures);
+                for (let i = 0; i < prediction.fixtures.length; i++) {
+                    thisWeeksFixtures.push(prediction.fixtures[i]);
+                }
+                console.log('Updateable fixtures:', thisWeeksFixtures);
+                res.render('game-pages/update-predictions', { data : thisWeeksFixtures });
             });
-        }
-        console.log('Predictions:', Prediction);
-        res.render('game-pages/predictions-posted.ejs');
-    });
-    router.put('/predictions', function(req, res) {
-        res.render('game-pages/update-predictions.ejs');
+        })
+        .catch(err => res.status(500).json({ message: 'Internal server error' }));
     });
     // =========================
     // RESULTS PAGE
@@ -225,5 +308,22 @@ module.exports = function(router) {
          // loads results.ejs file
     });
 
+    router.get('/all-predictions', isLoggedIn, function(req,res){
+        Predictions.find({},function(err,data){
+            if(!err){
+                res.send(data);
+            }
+        });
+    });
 
+
+};
+
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in session then carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if not authenticated, redirected to homepage
+    res.redirect('/');
 }
